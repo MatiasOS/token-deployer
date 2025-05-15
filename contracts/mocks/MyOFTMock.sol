97b245239da5d24f296d0c41c63c70ab3a3b3a3c
Mati OS <matias.ovejasmith@gmail.com>
2025-05-15 18:07:34 -0300

    fix test

--- contracts/mocks/MyOFTMock.sol
+++ contracts/mocks/MyOFTMock.sol
@@ -10,7 +10,7 @@ contract MyOFTMock is AlTokeOFT {
         string memory _symbol,
         address _lzEndpoint,
         address _delegate
-    ) AlTokeOFT(_name, _symbol, _lzEndpoint, _delegate, 100_000000000000000000) {}
+    ) AlTokeOFT(_name, _symbol, _lzEndpoint, _delegate, 0) {}
 
     function mint(address _to, uint256 _amount) public {
         _mint(_to, _amount);
